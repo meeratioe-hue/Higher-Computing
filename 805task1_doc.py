1cb61ecb8a9b25801d91ae8747f6c04b10b9aa99
Meera Tioe <M.Tioe@albynschool.org>
2025-10-30 09:58:53 +0000

    .

---
+++ 805task1_doc.py
@@ -0,0 +1,16 @@
+
+
+OwnsAnIPhone =["yes", "no", "no", "no", "yes","yes","yes","yes"]
+
+ValueToFind= "yes"
+counter = 0
+found = False
+
+while counter 
+for i in range (len(OwnsAnIPhone)):
+    if OwnsAnIPhone == "yes":
+         counter = counter + 1
+
+
+
+print (str(counter) + "owns Iphones")
