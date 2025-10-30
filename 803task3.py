28a533ba494837b1f8b63734a224efbd3d0c9c52
Meera Tioe <M.Tioe@albynschool.org>
2025-10-30 09:02:27 +0000

    .

--- 803task3.py
+++ 803task3.py
@@ -1,11 +1,14 @@
+#this is a procedure because it returns 0 parameters
 def module1(first, second, third):
   volume = first * second * third
   print("Volume =",volume)
 
+#this is a function because it returns 1 value
 def module2(monday):
   monday = monday/10
   return monday
 
+#this is a function because it returns 1 value
 def module3(blue, red):
   total = 0
   for counter in range(blue, red):
@@ -13,8 +16,11 @@ def module3(blue, red):
     print(total)
     return total
 
+#this is a function because it returns 1 value 
+# the % symbol means remainder. ex: 4%3 = 1
 def module4(up, down):
   middle = up % down
   top = int(up/down)
   total = middle + top
   return total
+
