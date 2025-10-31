992b39eff380d17b15f830265597741169578071
Meera Tioe <M.Tioe@albynschool.org>
2025-10-31 15:15:28 +0000

    .

---
+++ 805task2_doc.py
@@ -0,0 +1,10 @@
+
+
+PetPurchased = ["Dog", "Dog", "Cat", "Rabbit", "Hamster", "Cat", "Hamster", "Budgie"]
+
+LastUser = 0
+TargetAnimal = "Dog"
+for i in range(len(PetPurchased)):
+    if TargetAnimal == PetPurchased[i]:
+       LastUser = i + 1
+print(TargetAnimal, "was found in position:", LastUser)
---
+++ 805task3_doc.py
@@ -0,0 +1,6 @@
+PetPurchased = ["Dog", "Dog", "Cat", "Rabbit", "Hamster", "Cat", "Hamster", "Budgie"]
+counter = -1
+
+
+for loop in PetPurchased[i]:
+    
