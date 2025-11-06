4265ef1710aafa0c2943dbe50b4c6fdd9f7ac972
Meera Tioe <M.Tioe@albynschool.org>
2025-11-06 09:10:19 +0000

    .

--- 801task1.py
+++ 801task1.py
@@ -10,6 +10,21 @@ class person():
   
   BMIdetails = ???
 
+
+
+
+
+
+  from dataclasses import dataclass
+
+@dataclass
+class person():
+  height : float = 0.0
+  weight : float = 0.0
+  bmi : float = 0.0
+
+people = [person() for index in range(5)]
+
   BMIrecord[1].height = 1.75
   BMIrecord[1].weight = 70.5
   BMIrecord[1].bmi = BMIrecord[1].weight / BMIrecord[1].height **2 
