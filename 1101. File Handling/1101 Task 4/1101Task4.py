7615cf440fd0b9a527a713c71a3dfc213f1ec52b
Meera Tioe <M.Tioe@albynschool.org>
2025-11-07 15:03:37 +0000

    .

--- 1101. File Handling/1101 Task 4/1101Task4.py
+++ 1101. File Handling/1101 Task 4/1101Task4.py
@@ -1,9 +1,10 @@
 names = ["John","Joan","Mark","Michael"]
+BirthMonth = ["March", "April", "June", "December"]
 ages = [23,35,23,8]
 
 with open("names.txt","w") as wfile:
     for counter in range(0,len(names)):
-        wfile.write(names[counter] + ","+ str(ages[counter])+"\n")
+        wfile.write(names[counter] + ","+ str(BirthMonth[counter]) +"," +str(ages[counter])+"\n")
 
 # Run the program to see if it works. Now adapt the code slightly so that a third array
 #  (birth month) is added between lines 1 and 2 and also written to the CSV file.
