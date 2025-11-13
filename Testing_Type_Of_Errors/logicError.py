d2038e51029bfb7701a413b0da5fdc4db22a5854
Meera Tioe <M.Tioe@albynschool.org>
2025-11-13 09:11:38 +0000

    .

--- Testing_Type_Of_Errors/logicError.py
+++ Testing_Type_Of_Errors/logicError.py
@@ -1,7 +1,5 @@
 #logic error
 #is when there's an error in the logic of code such as using AND instead of OR
 
-
-Score = int(input("Input your test score:"))
-Score2 = int(input("input full marks:"))
-print((Score/Score2)/100, "%")
+total = int(input("enter a number to be divided:"))
+print("8")
