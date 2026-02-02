7a0eabe25421f105848df90d98dfb551938cfa11
Meera Tioe <M.Tioe@albynschool.org>
2026-02-02 11:43:39 +0000

    .

---
+++ Practice/2024/taskC_step2.py
@@ -0,0 +1,10 @@
+
+
+
+
+def ReadFromFileIntoParallelArrays():
+    return company, numEmployees, ceoSalary  
+def FindDifferenceBetweenCEOSalaries (copmany, ceoSalary) :
+    pass  
+def FindHighestNumOfEmployeesAndRange (numEmployees):
+    pass
