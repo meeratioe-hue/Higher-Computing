37cee6b40cac9d55dd59dc94352ffe4d77c2e287
meeratioe-hue <meeratioe@gmail.com>
2026-02-04 13:45:08 +0000

    Rename class and modify student file output
    Refactor student class and update file writing functionality.

--- mrsimpson-adapt-example1.py
+++ mrsimpson-adapt-example1.py
@@ -1,23 +1,25 @@
 from dataclasses import dataclass
 @dataclass
-class students():
+class student():
     studentID : int = 0
-    name: str =""
     email : str =""
+    password : str =""
     yearGroup : int = 10
 
-students = [student()for i in range (10)]
 
-def WriteDetailsOfTheWinningCustomer (finalorders, position):
+
+def writeAllStudentsToFile (theStudents):
 #3.1 Open new file ‘winningCustomer.txt’
-    with open("winningCustomer.txt", "w") as writefile:
-#3.2 If position is 0 or above then
-        if position >= 0 :
+    with open("Students.txt", "w") as writefile:
 #3.3 Write winning order number, email and cost to ‘winningCustomer.txt’
-            writefile.write(finalorders[position].orderNum + "," + finalorders[position].email + "," +  str(finalorders[position].cost))
-#3.4 Else
-        else:
-#3.5 Write ‘No winner’ to ‘winningCustomer.txt’
-            writefile.write("No winner")
+        writefile.write(theStudents[index].studentID + "," + str(theStudents.email) + "," +  str(theStudents.password) + "," + theStudents.yearGroup)
+
+
+
+# main
+# 1. setup students
+students = [student() for index in range(10)]
+# 2. write all students to file
+writeAllStudentsToFile(students)
 
 
