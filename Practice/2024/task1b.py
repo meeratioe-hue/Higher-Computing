ff07885fd800816625330c5a97fa663c741f2fec
Meera Tioe <M.Tioe@albynschool.org>
2026-02-25 11:46:32 +0000

    .

--- Practice/2024/task1b.py
+++ Practice/2024/task1b.py
@@ -19,6 +19,21 @@ print(company, numEmployees, ceoSalary)
 #Find and display the difference between the chosen company's CEO salary
 #and the highest CEO salary
 def FindDifferenceBetweenCEOs(company, ceoSalary):
+# 2.1 Ask user to enter the name of chosen company
+# 2.2 Set found to false
+# 2.3 Call findMaxPos function to return the position of highest CEO salary
+# 2.4 Loop for company array
+# 2.5   If current company is the chosen company
+# 2.6        Set found to true
+# 2.7       Set position to current index
+# 2.8 End if
+# 2.9 End loop
+# 2.10 If chosen company name is in list
+# 2.11   Subtract and store chosen company’s CEO salary from highest CEO salary
+# 2.12   Display message containing name of company with highest CEO salary, name of chosen company, and difference in salaries
+# 2.13 Else
+# 2.14  Display “Company not found”
+# 2.15 End if
     pass
 
 #Find and display the highest number of employees employed by a single
